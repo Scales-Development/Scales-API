59408127c88b8c42983e521e60c64ac476ddba2b
Cattu(Justin) <KirbyXstar@gmail.com>
2024-10-31 10:22:26 -0500

    unfinished go api for pulling basic data

--- main.go
+++ main.go
@@ -6,9 +6,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type testData struct {
+type testdata struct {
 	ID string `json:"id"`
 	Name string `json:"name"`
 	Status string `json:"status"`
-	
+}
+
+var testdatas = []testdata{
+	{ID: "1", Name: "Cattu", Status: "Denied" },
+	{ID: "2", Name: "Sourcetwo", Status: "Approved" },
+}
+
+func getDatas(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, testdatas)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/testdatas", getDatas)
+
+	router.Run("localhost:3000")
 }
