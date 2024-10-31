ee1d2a456b496910d79a6d7f9319f0ff60f1b823
Cattu(Justin) <KirbyXstar@gmail.com>
2024-10-31 10:16:29 -0500

    removal of js

--- data/guildSettingsSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const guildSchema = mongoose.Schema({
-    guildID: String,
-    ownerID: String,
-    appealChannel: String,
-
-}) // TODO: Finish database structure for guild schema for the backend
