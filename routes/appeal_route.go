58f271235981d521afd62b14fee13529022c03f3
Cattu(Justin) <KirbyXstar@gmail.com>
2024-11-11 07:00:08 -0600

    base file implentation of routes for users to grab and change information

---
+++ routes/appeal_route.go
@@ -0,0 +1,7 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func AppealRoute(app *fiber.App) {
+	// Route for interacting with appeal data and information
+}
---
+++ routes/user_route.go
@@ -0,0 +1,7 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func UserRoute(app *fiber.App) {
+	// Main route for storing and interacting with user data
+}
